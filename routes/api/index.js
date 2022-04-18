19e61334a7d17deb9599a8f8d06add7dd9252b2d
maxd66 <max.dunn63@gmail.com>
2022-04-18 01:01:00 -0700

    fixing test route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const strategyRoutes = require("./strategyRoutes");
 
-router.get("/test", (req, res) => {
-  res.json({ msg: "hey this works" });
-});
-
 router.use("/user", userRoutes);
 router.use("/strategy", strategyRoutes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
+router.get("/", (req, res) => {
+  res.json({ msg: "hey this works" });
+});
+
 router.use("/api", apiRoutes);
 
 module.exports = router;
