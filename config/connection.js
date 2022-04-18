fab0a196ab1b77305a4574e6b20814193713cb38
maxd66 <max.dunn63@gmail.com>
2022-04-18 01:15:03 -0700

    trying to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/valorant_random_db",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost/valorant_random_db",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("database connected"))
+  .catch((e) => console.log(e));
 
 module.exports = mongoose.connection;
